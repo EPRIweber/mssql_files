d4a23793327a4fccd66c48213a19ec532446f1e4
Isaac Weber <iweber@epri.com>
2025-07-05 15:50:20 -0400

    increasing tvp size

---
+++ sql/V31__Increasing_size.sql
@@ -0,0 +1,54 @@
+ALTER TABLE dbo.courses
+ALTER COLUMN course_code NVARCHAR(512);
+GO
+
+
+DROP PROCEDURE save_course_data;
+GO
+DROP TYPE CourseData_v2;
+GO
+
+CREATE TYPE dbo.CourseData_v2 AS TABLE (
+  course_code NVARCHAR(512),
+  course_title NVARCHAR(512) NOT NULL,
+  course_description NVARCHAR(MAX),
+  course_credits NVARCHAR(512)
+);
+GO
+
+
+
+/* Save Course Data from TVP */
+CREATE OR ALTER PROCEDURE dbo.save_course_data
+(
+    @source_id_in UNIQUEIDENTIFIER,
+    @course_data dbo.CourseData_v2 READONLY
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    MERGE dbo.courses WITH(HOLDLOCK) AS target
+    USING (
+        SELECT
+            @source_id_in AS sid,
+            course_code AS code,
+            course_title AS title,
+            course_description AS description,
+            course_credits AS credits
+        FROM
+            @course_data
+    ) AS source
+    ON (target.course_source_id = source.sid
+        AND COALESCE(target.course_code, '') = COALESCE(source.code, '')
+        AND target.course_title = source.title)
+    WHEN MATCHED THEN
+        UPDATE SET
+            course_description = source.description,
+            course_title = source.title,
+            course_code = source.code,
+            course_credits = source.credits
+    WHEN NOT MATCHED BY TARGET THEN
+        INSERT (course_source_id, course_code, course_title, course_description, course_credits)
+        VALUES (source.sid, source.code, source.title, source.description, source.credits);
+END;
+GO
